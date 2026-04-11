6d741836de8d8c2b59fd05965276201217375822
jakobflor3s <jakobflor3s@gmail.com>
2026-04-11 16:06:14 -0700

    Define card and hand structures with functions
    Added card game structures and function declarations.

--- functions.h
+++ functions.h
@@ -1,1 +1,37 @@
-jakob is silly
+#pragma once
+
+//Values
+static const char* suits[4] = { "Clubs", "Diamonds", "Spades", "Hearts" };
+
+static const char* values[13] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+//Structs
+typedef struct card{
+    int suit;
+    int value;
+    int dealt;
+    char lines[8][12];
+} Card;
+
+typedef struct hand {
+    Card held[20];
+    int amount;
+}Hand;
+
+//Functions
+
+void createCard(Card* c);
+
+void create(Card* deck[]);
+
+void printHand(Hand h, int amount);
+
+int draw(Card deck[]);
+
+void add_to_hand(int num, Hand* held, Card deck[]);
+
+int scoring(Hand held);
+
+char getCharacter();
+
+int getInteger();
